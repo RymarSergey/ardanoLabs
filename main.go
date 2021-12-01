d81a2ce345ef865aa7ead796a250dc6135fcda68
sergey <rymarsv@gmail.com>
2021-12-01 23:19:56 +0200

    add gracefully shutdown

--- main.go
+++ main.go
@@ -1,20 +1,67 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
-	h := http.HandlerFunc(Echo)
 
-	log.Println("we liastening on port:8000")
+	log.Println("main : started")
+	defer log.Println("main : completed")
 
-	if err := http.ListenAndServe("localhost:8000", h); err != nil {
-		log.Fatal(err)
+	//start api
+	api := http.Server{
+		Addr:         "localhost:8000",
+		Handler:      http.HandlerFunc(Echo),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	// Make a channel to listen for errors coming from the listener. Use a
+	// buffered channel so the goroutine can exit if we don't collect this error.
+	serverErrors := make(chan error, 1)
+
+	// Start the service listening for requests.
+	//if err := http.ListenAndServe("localhost:8000", h); err != nil {
+	//	log.Fatal(err)
+	//}
+	go func() {
+		log.Println("API listening on address:", api.Addr)
+		serverErrors <- api.ListenAndServe()
+	}()
+
+	//Make a channel to listen for interrupt or terminate signal from OS.
+	//Use the buffered channel because the signal package requeres it.
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	//===========================================
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error : listening and serving : %s", err)
+	case <-shutdown:
+		log.Println("main : start shutdown")
+
+		const timeout = 5 * time.Second
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+
+		err := api.Shutdown(ctx)
+		if err != nil {
+			log.Println("main : gracefull shutdown")
+			err = api.Close()
+		}
+		if err != nil {
+			log.Println("main : could not stop server grasefully")
+		}
 	}
 }
 
